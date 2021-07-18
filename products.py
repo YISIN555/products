0043105268d2999af88f21fee73bb00dc647db74
yi-sin <scaca52945@gmail.com>
2021-07-18 17:46:55 +0800

    add for loop for printing products

--- products.py
+++ products.py
@@ -8,6 +8,11 @@ while True:
 	# p.append(name)
 	# p.append(price)
 	products.append([name, price])  #等於7~8行
-print(products)
+print(products) #印出大清單
 
 products[0][0]
+
+
+for p in products:
+	print(p) #印出小清單
+	print(p[0], '的價格是', p[1])
